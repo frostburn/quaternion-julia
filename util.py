Fix mesh resolution and rotation issues

util.py
@@ -4,11 +4,11 @@ import quaternion
 
 
 def generate_mesh_slices(width, height, depth, center_w, center_x, center_y, center_z, zoom, rotation_theta, rotation_phi, rotation_gamma, rotation_beta, offset_x, offset_y, depth_dither=1):
-    ct, st = np.cos(rotation_theta), np.sin(rotation_phi)
+    ct, st = np.cos(rotation_theta), np.sin(rotation_theta)
     cp, sp = np.cos(rotation_phi), np.sin(rotation_phi)
     cg, sg = np.cos(rotation_gamma), np.sin(rotation_gamma)
     cb, sb = np.cos(rotation_beta), np.sin(rotation_beta)
-    zoom = 2**-zoom / height
+    zoom = 2**-zoom
 
     x = np.arange(width, dtype='float64') + offset_x
     y = np.arange(height, dtype='float64') + offset_y
@@ -16,12 +16,12 @@ def generate_mesh_slices(width, height, depth, center_w, center_x, center_y, cen
 
     x, y = np.meshgrid(x, y)
 
-    x = (2 * x - width) * zoom
-    y = (2 * y - height) * zoom
+    x = (2 * x - width) * zoom / height
+    y = (2 * y - height) * zoom / height
 
     for z_ in z:
         z_ += np.random.rand(*x.shape) * depth_dither - 0.5*depth_dither
-        z_ = (2 * z_ - depth) * zoom
+        z_ = (2 * z_ - depth) * zoom / depth
 
         # The screen coordinates have x as the real axis, so 'w' is a misnomer here.
         y_ = cb*y